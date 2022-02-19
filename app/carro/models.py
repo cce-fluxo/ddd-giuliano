c954e26ec4c1e7090776acdad36220831ed56496
GiuBontempo <giuliano.domiciano@gmail.com>
2022-02-19 08:56:52 -0300

    completa o models do carro

--- app/carro/models.py
+++ app/carro/models.py
@@ -1,6 +1,21 @@
+from markupsafe import _MarkupEscapeHelper
 from app.models import Automovel, db
 
 
 class Carro(Automovel):
+
+    __tablename__ = "carro"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     bancos = db.Column(db.String(10))
-    
+
+    def json(self):
+        return {
+            "marca" : self.marca,
+            "modelo" : self.modelo,
+            "ano" : self.ano,
+            "preco" : self.preco,
+            "motor" : self.motor,
+            "bancos" : self.bancos
+        }
