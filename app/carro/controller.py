e70437c72fbd5080c607519c966b55a4bc484fe3
GiuBontempo <giuliano.domiciano@gmail.com>
2022-02-22 19:39:21 -0300

    serializa carro e moto

--- app/carro/controller.py
+++ app/carro/controller.py
@@ -1,71 +1,42 @@
 from app.carro.models import Carro
-from flask import request
+from flask import request, jsonify
 from flask.views import MethodView
+from app.carro.schemas import CarroSchema
 
 
 class CarroG(MethodView):
     def post(self):
+        schema = CarroSchema()
         body = request.json
 
-        marca = body.get('marca')
-        modelo = body.get('modelo')
-        ano = body.get('ano')
-        cor = body. get('cor')
-        preco = body.get('preco')
-        motor = body.get('motor')
-        bancos = body.get('bancos')
+        carro = schema.load(body)
+        carro.save()
+        return schema.dump(carro)
 
-        if isinstance(marca, str) and isinstance(modelo, str) and isinstance(ano, str) and isinstance(cor, str) and isinstance(preco,float) and isinstance(motor, str) and isinstance(bancos, int):
-            carro = Carro(marca=marca, modelo=modelo, ano=ano, cor=cor, preco=preco, motor=motor, bancos=bancos)
-            carro.save()
-
-            return carro.json(),200
-
-        return {"code_status":"dados inválidos"},400
-
-    
     def get(self):
-
+        schema = CarroSchema()
         carros = Carro.query.all()
-        body = {}
-        
-        for carro in carros:
-            body[f"{carro.id}"] = carro.json()
-        return body
+        return jsonify(schema.dump(carros, many=True))
 
 
 class CarroID(MethodView):
     def get(self, id):
+        schema = CarroSchema()
         carro = Carro.query.get_or_404(id)
-        return carro.json()
+        return jsonify(schema.dump(carro))
 
-    def patch(self, id):
+
+    def patch(self,id):
         body = request.json
         carro = Carro.query.get_or_404(id)
+        schema = CarroSchema()
 
-        marca = body.get("marca", carro.marca)
-        modelo = body.get("modelo", carro.modelo)
-        ano = body.get("ano", carro.ano)
-        cor = body.get("cor", carro.cor)
-        preco = body.get("preco", carro.preco)
-        motor = body.get("motor", carro.motor)
-        bancos = body.get("bancos", carro.bancos)
-
-        if isinstance(marca, str) and isinstance(modelo, str) and isinstance(ano,str) and isinstance(cor,str) and isinstance(preco, float) and isinstance(motor, str) and isinstance(bancos, int):
-            carro.marca = marca
-            carro.modelo = modelo
-            carro.ano = ano
-            carro.cor = cor
-            carro.preco = preco
-            carro.motor = motor
-            carro.bancos = bancos
-
-            carro.update()
-            return carro.json(),200
+        carro = schema.load(body, instance=carro, partial=True)
+        carro.save()
+        return schema.dump(carro)
         
-        return {"code_status" : "dados inválidos"},400
 
     def delete(self, id):
         carro = Carro.query.get_or_404(id)
-        carro.delete()
-        return {"code_status":"carro deletado"},200
+        carro.delete(carro)
+        return {"code_status":"deletado"},200
--- app/moto/controller.py
+++ app/moto/controller.py
@@ -1,70 +1,42 @@
 from app.moto.models import Moto
-from flask import request
+from flask import request, jsonify
 from flask.views import MethodView
+from app.moto.schemas import MotoSchema
+
 
 class MotoG(MethodView):
     def post(self):
+        schema = MotoSchema()
         body = request.json
 
-        marca = body.get('marca')
-        modelo = body.get('modelo')
-        ano = body.get('ano')
-        cor = body.get('cor')
-        preco = body.get('preco')
-        motor = body.get('motor')
-
-        if isinstance(marca, str) and isinstance(modelo, str) and isinstance(ano, str) and isinstance(cor, str) and isinstance(preco, float) and isinstance(motor, str):
-            
-            moto = Moto(marca=marca, modelo=modelo, ano=ano, cor=cor, preco=preco, motor=motor)
-            moto.save()
-            return moto.json(),200
-
-        return {"code_status":"dados inválidos"},400
-
+        moto = schema.load(body)
+        moto.save()
+        return schema.dump(moto)
 
     def get(self):
+        schema = MotoSchema()
         motos = Moto.query.all()
-        body = {}
-
-        for moto in motos:
-            body[f"{moto.id}"] = moto.json()
-        return body
+        return jsonify(schema.dump(motos, many=True))
 
 
 class MotoID(MethodView):
     def get(self, id):
+        schema = MotoSchema()
         moto = Moto.query.get_or_404(id)
-        return moto.json()
+        return jsonify(schema.dump(moto))
 
 
     def patch(self,id):
         body = request.json
         moto = Moto.query.get_or_404(id)
+        schema = MotoSchema()
 
-        marca = body.get("marca", moto.marca)
-        modelo = body.get('modelo', moto.modelo)
-        ano = body.get('ano', moto.ano)
-        cor = body.get('cor', moto.cor)
-        preco = body.get('preco', moto.preco)
-        motor = body.get('motor', moto.motor)
-
-        if isinstance(marca, str) and isinstance(modelo, str) and isinstance(ano, str) and isinstance(cor, str) and isinstance(preco, float) and isinstance(motor, str):
-            
-            moto.marca = marca
-            moto.modelo = modelo
-            moto.ano = ano
-            moto.cor = cor
-            moto.preco = preco
-            moto.motor = motor
-
-            moto.update()
-            return moto.json(),200
-
-        return {"code_status":"dados inválidos"},400
-
+        moot = schema.load(body, instance=moto, partial=True)
+        moto.save()
+        return schema.dump(moto)
+        
 
     def delete(self, id):
         moto = Moto.query.get_or_404(id)
-        moto.delete()
-        return {"code_status":"moto deletada"},200
-    
+        moto.delete(moto)
+        return {"code_status":"deletado"},200
