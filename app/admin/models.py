ba3e41b96c60047efa64a44e85faa3c65d066d5e
GiuBontempo <giuliano.domiciano@gmail.com>
2022-03-07 18:42:46 -0300

    remove o limite de caracteres da senha_hash, o que deve resolver o problema

--- app/admin/models.py
+++ app/admin/models.py
@@ -9,7 +9,7 @@ class Admin(BaseModel):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     email = db.Column(db.String(70), nullable=False, unique=True)
-    senha_hash = db.Column(db.String(70), nullable=False)
+    senha_hash = db.Column(db.String(), nullable=False)
     role = 'admin'
 
     def json(self):
