777b412383ad3b653a31d7c52f1241445d17eecc
GiuBontempo <giuliano.domiciano@gmail.com>
2022-03-07 19:48:43 -0300

    refaz o migrations com a nova db

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv(path.join(env_path, 'env.env'))
 
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://asxcpewbleavhb:1cc88c333ad9e6d4c3407716350f6c6721f27872047957fbc962f6be68f0fa97@ec2-34-231-183-74.compute-1.amazonaws.com:5432/dcb8acgifegipn'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ffaivvqirixchs:88695f5c5e8202c9ae50b1e1b0d8c6790d445f85918ba9e74bd42dae72ce50ad@ec2-3-212-45-192.compute-1.amazonaws.com:5432/dd0s7oqouvfq13'
     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     JSON_SORT_KEYS = environ.get('JSON_SORT_KEYS')
 
