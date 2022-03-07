93ace805fda02136066d3e5e309bc346c9d11837
GiuBontempo <giuliano.domiciano@gmail.com>
2022-03-07 17:09:16 -0300

    tira o debug do environ para teste

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv(path.join(env_path, 'env.env'))
 
 
 class Config:
-    DEBUG = environ.get('DEBUG')
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://asxcpewbleavhb:1cc88c333ad9e6d4c3407716350f6c6721f27872047957fbc962f6be68f0fa97@ec2-34-231-183-74.compute-1.amazonaws.com:5432/dcb8acgifegipn'
     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     JSON_SORT_KEYS = environ.get('JSON_SORT_KEYS')
