b7c25b76695beee067c1536e1613a072d966318e
GiuBontempo <giuliano.domiciano@gmail.com>
2022-03-07 16:43:54 -0300

    adiciona procfile, requirements, e prepara o config para o deploy

--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
-from .sensive import Sensive as sensive
+from os import environ, path, pardir
+from dotenv import load_dotenv
+
+env_path = path.abspath(path.join(path.dirname(__file__), pardir))
+load_dotenv(path.join(env_path, 'env.env'))
 
 
 class Config:
-    DEBUG = sensive.DEBUG
-    SQLALCHEMY_DATABASE_URI = sensive.SQLALCHEMY_DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = sensive.SQLALCHEMY_TRACK_MODIFICATIONS
-    JSON_SORT_KEYS = sensive.JSON_SORT_KEYS
+    DEBUG = environ.get('DEBUG')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    JSON_SORT_KEYS = environ.get('JSON_SORT_KEYS')
 
-    JWT_SECRET_KEY = sensive.JWT_SECRET_KEY
+    JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
