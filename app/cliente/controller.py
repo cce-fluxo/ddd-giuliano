35e78120d2683a8a0628fad436d7198e54e7b538
GiuBontempo <giuliano.domiciano@gmail.com>
2022-03-15 23:15:50 -0300

    teste

--- app/cliente/controller.py
+++ app/cliente/controller.py
@@ -68,7 +68,7 @@ class ClienteLogin(MethodView):
         senha_inserida_hasheada = bcrypt.hashpw(senha.encode(), bcrypt.gensalt())
         check = False
         cliente = Cliente.query.filter_by(email=email).first()
-        return {"senha":f'{senha}', "senha_inserida_hash":f'{senha_inserida_hasheada}', "senha_cliente_hash":f'{cliente.senha_hash}'}
+        return {"senha":f'{senha}', "senha_inserida_hash":f'{senha_inserida_hasheada}', "senha_cliente_hash":f'{cliente.senha_hash}',"decode":senha_inserida_hasheada.decode()}
         if senha_inserida_hasheada == cliente.senha_hash:
             check = True
 
